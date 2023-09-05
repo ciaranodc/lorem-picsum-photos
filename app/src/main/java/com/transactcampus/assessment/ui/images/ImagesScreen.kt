bb1c21513680b6de0b7e58c4933682d19209561f
Ciaran <ciaranodc@gmail.com>
2023-09-05 23:43:55 +0100

    Filter images by author via dropdown selection

--- app/src/main/java/com/transactcampus/assessment/ui/images/ImagesScreen.kt
+++ app/src/main/java/com/transactcampus/assessment/ui/images/ImagesScreen.kt
@@ -2,22 +2,33 @@
 
 package com.transactcampus.assessment.ui.images
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.transactcampus.assessment.data.Image
-import com.transactcampus.assessment.ui.composables.CustomDropdownMenu
 
 @Composable
 fun ImagesScreen(imagesViewModel: ImagesViewModel) {
@@ -36,17 +47,72 @@ fun ImagesScreen(imagesViewModel: ImagesViewModel) {
 
 @Composable
 fun ImageScreenContent(images: List<Image>) {
-    val imagesList = images.reversed()
+    val defaultSelection = "Select author..."
     val authors: List<String> = images.map { it.author }.distinct()
+    var isExpanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf("Select author...") }
+
+    var imagesList = if (selectedText == defaultSelection) {
+        images.reversed()
+    } else {
+        images.reversed().filter { it.author == selectedText }
+    }
 
     Column {
-        CustomDropdownMenu(
-            menuItems = authors,
-            defaultSelection = "Select author..."
-        )
+//        CustomDropdownMenu(
+//            menuItems = authors,
+//            defaultSelection = "Select author..."
+//        )
+
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 10.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            ExposedDropdownMenuBox(
+                modifier = Modifier
+                    .weight(1f),
+                expanded = isExpanded,
+                onExpandedChange = {
+                    isExpanded = !isExpanded
+                }
+            ) {
+                TextField(
+                    value = selectedText,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded) },
+                    modifier = Modifier.menuAnchor()
+                )
+                ExposedDropdownMenu(
+
+                    expanded = isExpanded,
+                    onDismissRequest = { isExpanded = false }) {
+                    authors.forEach { item ->
+                        DropdownMenuItem(text = {
+                            Text(text = item)
+                        }, onClick = {
+                            selectedText = item
+                            isExpanded = false
+                        })
+                    }
+                }
+            }
+
+            Button(
+                shape = RoundedCornerShape(10),
+                onClick = {
+                    selectedText = "Select author..."
+                }
+            ) {
+                Text(text = "CLEAR")
+            }
+        }
+
         LazyVerticalGrid(
             columns = GridCells.Fixed(1),
         ) {
+
             items(
                 // reverse photos list because first few photos are terrible but last few are lovely
                 items = imagesList,
