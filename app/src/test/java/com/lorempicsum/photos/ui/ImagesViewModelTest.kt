f292c83a86f9cab4f48c479757c22b6e59fc5aec
Ciaran <ciaranodc@gmail.com>
2023-10-03 17:07:38 +0100

    Fix unit test

--- app/src/test/java/com/lorempicsum/photos/ui/ImagesViewModelTest.kt
+++ app/src/test/java/com/lorempicsum/photos/ui/ImagesViewModelTest.kt
@@ -1,22 +1,43 @@
 package com.lorempicsum.photos.ui
 
 import androidx.paging.testing.asSnapshot
+import com.lorempicsum.photos.CoroutinesTestRule
 import com.lorempicsum.photos.data.source.FakeImageRepository
 import com.lorempicsum.photos.ui.images.ImagesViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
+import kotlin.coroutines.EmptyCoroutineContext
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class ImagesViewModelTest {
-
     private var repository = FakeImageRepository()
     private var viewModel = ImagesViewModel(repository)
 
-    @Test
-    fun whenImagesAreLoadedInViewModel_thenCorrectNumberOfImagesIsPresent() = runTest {
-        val images = viewModel.images
-        val imagesSnapshot = images.asSnapshot()
+    @get:Rule
+    var coroutinesTestRule = CoroutinesTestRule()
 
-        assertEquals(30, imagesSnapshot.size)
+    @Test
+    fun whenImagesAreLoadedInViewModel_thenCorrectNumberOfImagesIsPresent() {
+        coroutinesTestRule.testDispatcher.dispatch(EmptyCoroutineContext) {
+            runTest {
+                val images = viewModel.images
+                val imagesSnapshot = images.asSnapshot()
+
+                assertEquals(30, imagesSnapshot.size)
+            }
+        }
+    }
+
+
+    @Test
+    fun whenImagesAreLoadedInViewModel_thenDetailsOfFirstImageAreCorrect() = runTest {
+        val firstImage = viewModel.images.asSnapshot()[0]
+
+        assertEquals("0", firstImage.id)
+        assertEquals("Alejandro Escamilla", firstImage.author)
+        assertEquals("https://picsum.photos/id/0/5000/3333", firstImage.downloadUrl)
     }
 }
